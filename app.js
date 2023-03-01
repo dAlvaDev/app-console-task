fbe4201e5a396896d0862859faa026017206929a
Denis <da962525089@gmail.com>
2023-02-28 19:30:03 -0500

    Crear interacción con la consola

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
-
 require('colors');
+
+const { inquirerMenu } = require('./helpers/inquirer');
+
 console.clear();
 
 const main = async() => {
@@ -9,10 +10,11 @@ const main = async() => {
     let opt = '';
 
     do {
-        opt = await mostrarMenu();
+        opt = await inquirerMenu();
         console.log({opt});
-        await pausa();
         
+        if( opt !== '0' ) await pausa();
+
     } while (opt !== '0');
 
 
---
+++ helpers/inquirer.js
@@ -0,0 +1,31 @@
+const inquirer = require('inquirer');
+require('colors');
+
+const preguntas = [
+    {
+        type: 'list',
+        name: 'opcion',
+        message: '¿Qué desea hacer?',
+        choices: [
+            'opt1', 'opt2', 'opt3'
+            
+        ]
+    }
+];
+
+const inquirerMenu = async() => {
+
+    console.clear();
+    console.log('=========================='.green);
+    console.log('  Seleccione una opción'.white );
+    console.log('==========================\n'.green);
+    // Interacción con la consola
+    const  opt = await inquirer.prompt(preguntas);
+
+    return opt;
+}
+
+module.exports = {
+    inquirerMenu,
+    
+}
